2a5afacdacb044fb8bc3f6114e38b103ad7c7237
Sahil <thisissahil.2002@gmail.com>
2026-03-31 02:16:35 +0530

    feat(backend): add alembic setup and auth improvements

---
+++ backend/routers/auth.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from database import get_db
+from schemas.user import UserCreate, UserOut, Token
+from services.auth_service import create_user, login_user
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+@router.post("/signup", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+def signup(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = create_user(db,user)
+    if not new_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered."
+        )
+    return new_user
+
+@router.post("/login", response_model=Token)
+def login(user: UserCreate, db: Session = Depends(get_db)):
+    result = login_user(db,user.email,user.password)
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_201_UNAUTHORIZED,
+            detail="Invalid Credentials.",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    return result
---
+++ backend/services/auth_service.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from models.user import User
+from schemas.user import UserCreate
+from core.security import hash_passoword, verify_password, create_access_token
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def create_user(db: Session, user: UserCreate):
+    existing_user = get_user_by_email(db,user.email)
+    if existing_user:
+        return None
+    hashed = hash_passoword(user.password)
+    new_user = User(email = user.email, hash_passoword = hashed)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db, email)
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    return user
+
+def login_user(db: Session, email: str, password: str):
+    user = authenticate_user(db, email, password)
+    if not user:
+        return None
+    token = create_access_token(data={"sub": user.email})
+    return {"access_token": token, "token_type": "bearer"}
